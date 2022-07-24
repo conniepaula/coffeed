1a9fda809e22d160abd937db421696ad674a7d01
Connie <105062735+conniepaul@users.noreply.github.com>
2022-07-24 12:33:43 +0100

    Add comment section + fixes previous files

---
+++ src/components/Comment.jsx
@@ -0,0 +1,36 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import React from "react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img className="avatar" src="https://i.ibb.co/k32Vkd6/man1.jpg" alt="" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorTime}>
+              <strong>Mary Hornsby</strong>
+              <time
+                title="23 July 2022 at 17:22"
+                dateTime="2022-07-23 17:22:14"
+              >
+                17:22
+              </time>
+            </div>
+            <button title="Delete Comment">
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>That's true, Andy! Always have Coffika on my lunch break.</p>
+        </div>
+        <footer>
+        <button>
+          <ThumbsUp size={20} />
+          Thumbs Up <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
